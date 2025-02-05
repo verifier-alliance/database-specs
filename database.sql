17af45c9530cd9e37b4d9d0f7fb51c3e0c30f60c
Rim Rakhimov <rimrakhimov@gmail.com>
2025-02-05 13:35:36 +0400

    feat(verified_contracts): add check that the key is not empty in 'values_immutables' check

--- database.sql
+++ database.sql
@@ -706,13 +706,16 @@ BEGIN
         RETURN true;
     END IF;
 
-    IF is_jsonb_object(obj -> 'immutables') THEN
-        RETURN bool_and(are_valid_values)
-            FROM (SELECT is_jsonb_string(value) AND is_valid_hex(value ->> 0, '+') as are_valid_values
-                  FROM jsonb_each(obj -> 'immutables')) as subquery;
-    ELSE
+    IF NOT is_jsonb_object(obj -> 'immutables') THEN
         RETURN false;
     END IF;
+
+    RETURN bool_and(
+        length(key) > 0 AND
+        is_jsonb_string(value) AND
+        is_valid_hex(value ->> 0, '+')
+    )
+    FROM jsonb_each(obj -> 'immutables');
 END;
 $$ LANGUAGE plpgsql;
 
